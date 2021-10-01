d11721b49119edaf807c4b9cf9af8f6e76ae3c40
santi19999 <santiherrera1999@gmail.com>
2021-09-30 21:44:22 -0300

    Cada vez que un usuario nuevo utilice nuestro programa, vamos a guardar un archivo con sus datos.

--- MiRedS4c-Funciones.py
+++ MiRedS4c-Funciones.py
@@ -1,43 +1,104 @@
+#Recordemos que en este módulo están todos las funciones adicionales que hemos creado.
+import S5Red as Red
+#El módulo 'os' nos permitirá consultar si un archivo existe.
+import os
 
-import S4Red as Red
+def leer_archivo(nombre):
+    nombre = archivo_usuario.readline()
+    edad = int(archivo_usuario.readline())
+    estatura = float(archivo_usuario.readline())
+    estatura_m = int(estatura)
+    estatura_cm = int( (estatura - estatura_m)*100 )
+    sexo = archivo_usuario.readline()
+    pais = archivo_usuario.readline()
+    num_amigos = int(archivo_usuario.readline())
+    estado = archivo_usuario.readline()
 
-#Aquí empieza el programa principal.
+def escribir_archivo():
+    archivo_usuario.write(nombre+"\n")
+        archivo_usuario.write(str(edad)+"\n")
+        archivo_usuario.write(str(estatura_m + estatura_cm/100)+"\n")
+        archivo_usuario.write(sexo+"\n")
+        archivo_usuario.write(pais+"\n")
+        archivo_usuario.write(str(num_amigos)+"\n")
+        archivo_usuario.write(estado+"\n")
+
 
 Red.mostrar_bienvenida()
-(nombre, edad, estatura_m, estatura_cm, num_amigos) = Red.obtener_datos()
+nombre = Red.obtener_nombre()
+print("Hola ", nombre, ", bienvenido a Mi Red")
 
-print("Muy bien,", nombre, ". Entonces podemos crear un perfil con estos datos.")
-Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, num_amigos)
-print("Ya podemos preguntar, recordar y calcular datos. Esperamos que disfrutes con Mi Red")
-print("--------------------------------------------------")
+#Verificamos si el archivo existe
+if os.path.isfile(nombre+".user"):
+    #Esto lo hacemos si ya había un usuario con ese nombre
+    print("Leyendo datos de usuario", nombre, "desde archivo.")
+    archivo_usuario = open(nombre+".user","r")
+    lectura_archivo(nombre) 
+    
+    #Una vez que hemos leido los datos del usuario no debemos olvidar cerrar el archivo
+    archivo_usuario.close()
 
+else:
+    #En caso que el usuario no exista, consultamos por sus datos tal como lo hacíamos antes
+    print()
+    edad = Red.obtener_edad()
+    (estatura_m, estatura_cm) = Red.obtener_estatura()
+    sexo = Red.obtener_sexo()
+    pais = Red.obtener_pais()
+    num_amigos = Red.obtener_num_amigos()
+    estado = ""
+
+#En ambos casos, al llegar aquí ya conocemos los datos del usuario
+print("Muy bien. Estos son los datos de tu perfil.")
+Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos)
+Red.mostrar_mensaje(nombre, None, estado)
+
+#Ahora podemos mostrar el menu y consultar las opciones
 opcion = 1
 while opcion != 0:
     opcion = Red.opcion_menu()
     if opcion == 1:
-        mensaje = Red.obtener_mensaje()
-        Red.mostrar_mensaje(nombre, None, mensaje)
+        estado = Red.obtener_mensaje()
+        Red.mostrar_mensaje(nombre, None, estado)
     elif opcion == 2:
-        mensaje = Red.obtener_mensaje()
+        estado = Red.obtener_mensaje()
         for i in range(num_amigos):
             nombre_amigo = input("Ingresa el nombre de tu amigo o amiga: ")
-            Red.mostrar_mensaje(nombre, nombre_amigo, mensaje)
+            Red.mostrar_mensaje(nombre, nombre_amigo, estado)
     elif opcion == 3:
-        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, num_amigos)
+        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos)
     elif opcion == 4:
-        nombre = Red.obtener_nombre()
         edad = Red.obtener_edad()
         (estatura_m, estatura_cm) = Red.obtener_estatura()
+        sexo = Red.obtener_sexo()
+        pais = Red.obtener_pais()
         num_amigos = Red.obtener_num_amigos()
-        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, num_amigos)
+        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos)
     elif opcion == 0:
-        print("Has decidido salir.")
+        print("Has decidido salir. Guardando perfil en ",nombre+".user")
+        archivo_usuario = open(nombre+".user","w")
+        
+        #Una vez que hemos escrito todos los datos del usuario en el archivo, no debemos olvidar cerrarlo
+        archivo_usuario.close()
+        print("Archivo",nombre+".user","guardado")
+
+
 
 print("Gracias por usar Mi Red. ¡Hasta pronto!")
 
-#Como puedes ver, la parte principal de tu programa ahora es mucho más corto.
-#Gran parte de la funcionalidad está encapsulada en funciones, y estas funciones están definidas
-# en otro archivo que hemos importado.
+#Cuando ejecutes este programa por primera vez, verás que se crea un archivo nuevo en tu computador
+#cada vez que ingresas con el nombre de un usuario nuevo. Prueba a ingresar
+#con un nombre de usuario que ya habías usado antes.
 
-#Al usar funciones podemos concentrarnos en partes más específicas de nuestro código,
-# y nos ayuda a razonar a un nivel más alto.
+#Este programa es bastante más completo que los que teníamos antes, sin embargo aún tiene cosas
+#por mejorar. Por ejemplo, ¿qué ocurre si el archivo está mal escrito, o si le falta alguna línea?
+#¿Qué ocurre si en una ocasión ingreso mi nombre con mayúsculas, y en otra ocasión lo hago con minúsculas?
+#
+#Te invitamos a corregir dos detalles en este programa
+#1. Al leer las líneas del archivo, siempre se leen como últimos caracteres, algunos caracteres blancos como
+#   espacios y el caracter de cambio de línea ('\n'). Esto hace que los nombres de archivos creados incluyan
+#   estos caracteres adicionales. Puedes utilizar los métodos de str para eliminar este tipo de caracteres
+#   que llamamos "no imprimibles"
+#2. Utiliza tu conocimiento de funciones para crear funciones que lean desde un archivo,
+#   y retornen el conjunto de datos leídos, de manera de encapsular el proceso de lectura y escritura,
+#   y reducir el tamaño de tu código.
