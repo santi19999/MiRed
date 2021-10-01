b72a6b64a6c1c878dd5cb8af73695655f51b7b46
Sergio Santiago Herrera <43582762+santi19999@users.noreply.github.com>
2021-09-30 23:06:03 -0300

    Create lista.py
    Funciones y operaciónes que se pueden hacer con las listas

---
+++ lista.py
@@ -0,0 +1,3 @@
+texto = input("Ingresa los elemenetos de la lista")
+
+lista = texto.split(",") # Esto quiere decir que cada vez que ingrese una coma , es un elemento nuevo que se va a agregar a la lista.
